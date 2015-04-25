7f92d92dc65b171d6b9f4ef0c0558a5a5fbacdab
Kucherenko-Alexander <kuchkiper@gmail.com>
2015-04-25 18:43:36 +0300

    Update console.py

--- console.py
+++ console.py
@@ -1,33 +1,36 @@
 from tkinter import *
+
 def inscription(event):
     global line
-    a = text1.get(str(line), END)
-    analize(a)        
-    line += 1.0
+    exec(text1.get(str('5.0'), END))      
     
 
-def analize(a):
-    global line
-    if a == 'RunRight()\n':
-        RunRight()
-    elif a == 'RunLeft()\n':
-        RunLeft()
-    elif a == 'RunUp()\n':
-        RunUp()        
-    elif a == 'RunDown()\n':
-        RunDown()            
-    elif a == 'Attack()\n':
-        Attack()
-    else:
-        text1.insert(str(line)[:2] + str(len(a) + 1), ' Wrong Command')
+def RunRight():
+    pass
+
+
+def RunLeft():
+    pass
+
+
+def RunDown():
+    pass
+
+
+def RunUp():
+    pass
+        
         
 root = Tk()
 root.title('CONSOLE')
-text1 = Text(root, height=10, width=50, font = 'Arial 14', wrap=WORD, bg ='blue', fg='red')
+text1 = Text(root, height=15, width=75, font = 'Arial 14', wrap=WORD, bg ='mint cream', fg='red')
 text1.insert('1.0', 'Here is your console\n')
-text1.insert('2.0', 'You should use commands:\n')
-text1.insert('3.0', 'RunRight(), RunLeft(), RunUp(), RunDown(), Attack()\n')
-line = 4.0
+text1.insert('2.0', 'You should use python commands\n')
+text1.insert('3.0', 'And you must use commands:\n')
+text1.insert('4.0', 'RunUp(), RunDown(), RunLeft(), RunRight()\n')
+ready = Button(root, text = 'READY', bg = 'orange', font = 'arial 36')
+c = 0
 text1.pack()
-text1.bind('<Return>', inscription)
+ready.pack(fill = 'both')
+ready.bind('<Button-1>', inscription)
 root.mainloop()
